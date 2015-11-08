0797e6bc2b2217c74b3a2349acc9067aec782e23
YANG MING <thermosym@gmail.com>
2015-11-08 14:15:33 +0800

    create java demo project

---
+++ stream-api-demo/src/main/java/ym/dev/stream_api_demo/App.java
@@ -0,0 +1,13 @@
+package ym.dev.stream_api_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
